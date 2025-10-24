5c173f825fbc443195d4b3e3b9a65e72378281e3
Hector Coronado <hectorcoronado.developer@gmail.com>
2025-10-24 15:44:51 -0700

    [build] remove import for clean-css

--- build.js
+++ build.js
@@ -5,7 +5,6 @@ import { execSync } from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import CleanCSS from 'clean-css';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
